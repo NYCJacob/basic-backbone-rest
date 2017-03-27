7e3ea0f67c5ef66dfd38aca4baa45b28767894a9
jsherman <jacob@jbsherman.com>
2017-03-26 21:10:47 -0400

    Bug: file selector not working

---
+++ site/js/collections/library.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jsherman on 3/26/17.
+ */
+// site/js/collections/library.js
+
+var app = app || {};
+
+app.Library = Backbone.Collection.extend({
+    model: app.Book
+});
---
+++ site/js/models/book.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jsherman on 3/26/17.
+ */
+// site/js/models/book.js
+
+var app = app || {};
+
+app.Book = Backbone.Model.extend({
+    defaults: {
+        coverImage: 'img/placeholder.png',
+        title: 'No title',
+        author: 'Unknown',
+        releaseDate: 'Unknown',
+        keywords: 'None'
+    }
+});
